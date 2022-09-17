b92a53b7bb7e8a53d90f122617e3bbc3282c7590
RaquelDommingues <110355736+RaquelDommingues@users.noreply.github.com>
2022-09-18 00:53:42 +0100

    Current Emoji updated

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
+    let iconElement = document.querySelector("#current-emoji");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description)
 }
 
 let apiKey = "a5dd247415f25bfa34422eabd0fe171e";
-
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metric`;
+let city = "New York"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl)
 
@@ -16,6 +19,7 @@ axios.get(apiUrl).then(displayTemperature)
 //Date
 
 function displayDate(response){
+
     let date = new Date();
     let offset = response.data.timezone * 1000;
     let utc = date.getTime() + (date.getTimezoneOffset() * 60000);
@@ -23,6 +27,7 @@ function displayDate(response){
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     let day = days[newDate.getDay()];
     let hours = newDate.getHours();
+
     if (hours < 10) {
     hours = `0${hours}`;
     }
@@ -30,10 +35,10 @@ function displayDate(response){
     if (minutes < 10) {
     minutes = `0${minutes}`;
     }
+
     let time = document.querySelector("#date");
 
     time.innerHTML = `${day} ${hours}:${minutes}`;
 }
-
 
 axios.get(apiUrl).then(displayDate)
