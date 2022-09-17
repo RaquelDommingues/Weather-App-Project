f22f731161d3aeaf0f3aaf14dbb5623182039f32
RaquelDommingues <110355736+RaquelDommingues@users.noreply.github.com>
2022-09-18 00:32:20 +0100

    Date related functions updated

--- src/app.js
+++ src/app.js
@@ -12,3 +12,28 @@ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid
 console.log(apiUrl)
 
 axios.get(apiUrl).then(displayTemperature)
+
+//Date
+
+function displayDate(response){
+    let date = new Date();
+    let offset = response.data.timezone * 1000;
+    let utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+    let newDate = new Date(utc + (offset));
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = days[newDate.getDay()];
+    let hours = newDate.getHours();
+    if (hours < 10) {
+    hours = `0${hours}`;
+    }
+    let minutes = newDate.getMinutes();
+    if (minutes < 10) {
+    minutes = `0${minutes}`;
+    }
+    let time = document.querySelector("#date");
+
+    time.innerHTML = `${day} ${hours}:${minutes}`;
+}
+
+
+axios.get(apiUrl).then(displayDate)
